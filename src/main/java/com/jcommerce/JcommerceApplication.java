5cdb225e26f11029c0a844c4f6a17baca78001f0
Riyazuddin M <riyaz4ug@gmail.com>
2017-03-08 16:55:12 +1100

    Initial project commit

---
+++ src/main/java/com/jcommerce/JcommerceApplication.java
@@ -0,0 +1,12 @@
+package com.jcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcommerceApplication.class, args);
+	}
+}
